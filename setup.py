9157796971ef6aa1239c0524d64c82dc2141f034
Agent1997 <rommelmalked@gmail.com>
2023-07-06 10:51:35 -0400

    renamed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r', encoding='utf-16') as requirements_file:
     install_requires = requirements_file.readlines()
       
 setup(
-    name='autocore',
+    name='mrrf_core',
     version='1.0.0',
     author='Agent 1997',
     packages=find_packages(),
